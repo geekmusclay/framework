976a0d19ae6e3fecefa5c454fcf6903c8df63074
Cédric Noël <cedric.noel.paris@gmail.com>
2022-12-26 12:28:13 +0100

    master: added factory for twig renderer

--- src/Factory/TwigRendererFactory.php
+++ src/Factory/TwigRendererFactory.php
@@ -12,8 +12,11 @@ use Twig\Loader\FilesystemLoader;
 
 class TwigRendererFactory
 {
-    public function __invoke(string $path, array $config = [], ContainerInterface $container): TwigRenderer
+    public function __invoke(ContainerInterface $container): TwigRenderer
     {
+        $path   = $container->get('view.path');
+        $config = $container->get('twig.config');
+
         $this->loader = new FilesystemLoader($path);
         $this->twig   = new Environment($this->loader, $config);
         $this->twig->addExtension($container->get(TwigExtension::class));
