98e9124d4be9ff0f8f3a80616503a3261ab6b149
cedric <cedric@vision-si.com>
2023-03-28 09:58:36 +0200

    master: added json response

--- src/Common/AbstractController.php
+++ src/Common/AbstractController.php
@@ -5,10 +5,15 @@ declare(strict_types=1);
 namespace Geekmusclay\Framework\Common;
 
 use Geekmusclay\Framework\Interfaces\RendererInterface;
+use Geekmusclay\Router\Core\JsonResponse;
 use GuzzleHttp\Psr7\Response;
 
 abstract class AbstractController
 {
+    const JSON_HEADERS = [
+        'Content-Type' => 'application/json'
+    ];
+
     protected RendererInterface $renderer;
 
     public function __construct(RendererInterface $renderer)
@@ -24,5 +29,19 @@ abstract class AbstractController
         $body = $this->renderer->render($path, $params);
 
         return new Response(200, $headers, $body);
+    }
+
+    protected function json(
+        array $body = [],
+        int $status = 200,
+        array $headers = [],
+        string $version = '1.1',
+        ?string $reason = null
+    ): JsonResponse {
+        $data = json_encode($body);
+        $headers = array_merge(self::JSON_HEADERS, $headers);
+        $headers = array_unique($headers);
+
+        return new JsonResponse($status, $headers, $data, $version, $reason);
     }
 }
