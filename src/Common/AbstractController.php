7ff70beabe84d35b3105155e00c4731bb8482135
cedric <cedric@vision-si.com>
2023-04-17 14:13:03 +0200

    master: added redirection function

--- src/Common/AbstractController.php
+++ src/Common/AbstractController.php
@@ -6,7 +6,9 @@ namespace Geekmusclay\Framework\Common;
 
 use Geekmusclay\Framework\Interfaces\RendererInterface;
 use Geekmusclay\Router\Core\JsonResponse;
+use Geekmusclay\Router\Interfaces\RouterInterface;
 use GuzzleHttp\Psr7\Response;
+use Psr\Http\Message\ServerRequestInterface;
 
 abstract class AbstractController
 {
@@ -14,13 +16,44 @@ abstract class AbstractController
         'Content-Type' => 'application/json'
     ];
 
+    /** @var RendererInterface $renderer Defdault renderer */
     protected RendererInterface $renderer;
 
-    public function __construct(RendererInterface $renderer)
+    /** @var RouterInterface $router Application router */
+    protected RouterInterface $router;
+
+    /**
+     * Controller constructor
+     *
+     * @param RendererInterface $renderer Defdault renderer
+     * @param RouterInterface   $router   Application router
+     */
+    public function __construct(RendererInterface $renderer, RouterInterface $router)
     {
         $this->renderer = $renderer;
+        $this->router   = $router;
     }
 
+    /**
+     * Redirection to a named route.
+     *
+     * @param ServerRequestInterface $request The server request
+     * @param string                 $name    The name of the route to execute
+     * @return mixed
+     */
+    protected function redirect(ServerRequestInterface $request, string $name): mixed
+    {
+        return $this->router->redirect($request, $name);
+    }
+
+    /**
+     * Render given file.
+     *
+     * @param string $path    Path to the file
+     * @param array  $params  Params to pass to the view
+     * @param array  $headers Headers of the response
+     * @return Response
+     */
     protected function render(
         string $path,
         array $params  = [],
@@ -31,6 +64,16 @@ abstract class AbstractController
         return new Response(200, $headers, $body);
     }
 
+    /**
+     * Return a json response for api call.
+     *
+     * @param array       $body    The body of the response
+     * @param integer     $status  The response status code
+     * @param array       $headers The response headers
+     * @param string      $version Protocol version
+     * @param string|null $reason  Reason phrase (when empty a default will be used based on the status code)
+     * @return JsonResponse
+     */
     protected function json(
         array $body = [],
         int $status = 200,
